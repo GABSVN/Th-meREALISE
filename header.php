d73ad4dab2ff8bc1960953b21b560136dfd19019
iNiello <99719850+iNiello@users.noreply.github.com>
2022-07-21 10:16:09 +0200

    feat: #1 prototype header

--- header.php
+++ header.php
@@ -1,2 +1,42 @@
 <?php
-echo 'salut à tutti';
+//echo 'salut à tutti';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<div class=" header">
+    <title>Document</title>
+    <style>
+        div {
+            background-color: hotpink;
+            height: 100px;
+        }
+
+        p {
+            background-color: orange;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        <p>Header</p>
+    </div>
+    
+    <div class="content">
+        <p>Content</p>
+    </div>
+    <div class="ads">
+        <p>Ads</p>
+    </div>
+    <div class="footer">
+        <p>Footer</p>
+    </div>
+    <!---->
+</body>
+
+</html>
