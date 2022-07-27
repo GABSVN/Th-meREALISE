fed6cc2148b50407f6a27eb3a6624a6bea5c36cb
Diogo Ferreira Oliveira <99719843+diogofoliveir@users.noreply.github.com>
2022-07-27 11:56:06 +0200

    newtheme

--- header.php
+++ header.php
@@ -2,20 +2,30 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <?php wp_head() ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 <header class="banner" role="banner">
      <nav class="navbar " style="background-color: #e3f2fd; height: 100px;">
         <div class="container-fluid">
-          <a class="navbar-brand">Realise</a>
+        
+        <a class="navbar-brand" >Realise </a>
+        <?php
+wp_nav_menu( array( 
+    'theme_location' => 'my-custom-menu', 
+    'container_class' => 'custom-menu-class' )); 
+?>
           <form class="d-flex" role="search">
+      
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
+         
         </div>
       </nav>
     </header>
