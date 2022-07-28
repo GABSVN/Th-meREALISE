904c186ef6eeaf7081bee292649021629987f603
Diago Dryson <cortezdiago@gmail.com>
2022-07-28 09:07:12 +0200

    Update archive.php

--- archive.php
+++ archive.php
@@ -24,12 +24,29 @@
         <h2><?php the_title(); ?></h2>
   
         <?php the_post_thumbnail(); ?>
+
+        <!-- Pour afficher la date, on va utiliser the_time() même si on dispose d'une autre fonction
+la fonction get_option() permet de récupérer la valeur indiquée dans Réglages > général > Format de date -->
         <p class="post__meta">
                 Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
                 par <?php the_author(); ?> • <?php comments_number(); ?>
             </p>
 
+            <!-- La fonction the_date() affichera qu’une seule fois la même date si plusieurs articles sont publiés le même jour, seulement le premier affichera la date. En réalité on s’en sert pour un classement chronologique ou on affiche d’abord une date, puis la liste des articles publiés ce jour-ci -->
+            <?php the_excerpt(); ?>
+              
+      		<p>
+                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+            </p>
+		</article>
+
+	<?php endwhile; endif; ?>
+
+    <?php get_template_part( 'parts/newsletter' ); ?>
+
+<?php get_footer(); ?>
+
 <!-- https://wpchannel.com/wordpress/tutoriels-wordpress/creer-page-darchives-articles-classes-categories/
  https://code.tutsplus.com/articles/create-a-wordpress-archives-template-for-your-theme--wp-23384
-https://wpfr.net/support/sujet/trouver-le-fichier-archive-php/
--->
+https://wpfr.net/support/sujet/trouver-le-fichier-archive-php/-->
+
