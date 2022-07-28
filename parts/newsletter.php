f0169674c671c7e5aa67dcefbab3e91126020d79
Diago Dryson <cortezdiago@gmail.com>
2022-07-28 11:03:02 +0200

    Update newsletter.php

--- parts/newsletter.php
+++ parts/newsletter.php
@@ -2,4 +2,5 @@
     <p>Le thème Réalise vous propose des offres exclusives, des actus WordPress et du contenu bonus en avant-première</p>
     <input type="email" name="email" placeholder="E-mail">
     <input type="submit" value="Je m'abonne">
+    
 </form>
