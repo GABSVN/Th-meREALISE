2ed889cadaa3ac046891c3f781331237a12c63dc
iNiello <99719850+iNiello@users.noreply.github.com>
2022-07-29 11:53:10 +0200

    wip: #3 add arguments sidebar

--- functions.php
+++ functions.php
@@ -1,10 +1,23 @@
 
 <?php
+// Mettre la feuille de style css
+function themerealise_style_css(){
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+
+add_action('wp_enqueue_scripts', 'themerealise_style_css');
+
+// ajouter l'inteface widget dans le back office
 function themerealise_register_widget(){
 
-    register_sidebar(array(
-        'name'  => __( 'Primary Sidebar', 'themerealise' ),
-        ) );
+    register_sidebar([
+        'name'  => 'aside-droite',
+        'id' => 'aside-droite',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ]);
 };
 
 // enregistrement de la sidebar grace au hook widgets_init
